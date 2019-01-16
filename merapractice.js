8f2f2f2aa8c5a603a9ce15191a51e42da81e8cdb
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-16 06:03:09 +0000

    Automatically backed up by Learn

--- merapractice.js
+++ merapractice.js
@@ -1,7 +1,7 @@
 function funkyFunction() {
   var innerVariable = "I'm sort of a secret.";
  
-  return function innerScope() {
+  return function() {
     var inaccessible = "Nothing can touch me.";
  
     return "Funky";
