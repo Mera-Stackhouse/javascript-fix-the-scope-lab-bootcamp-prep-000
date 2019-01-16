263c8cf967dccf400840bbb98aaad0d6623cd779
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-16 06:01:47 +0000

    Automatically backed up by Learn

--- merapractice.js
+++ merapractice.js
@@ -1,10 +1,10 @@
-function funkyFunction() {
+function outerFunction() {
   var innerVariable = "I'm sort of a secret.";
  
   return function innerScope() {
     var inaccessible = "Nothing can touch me.";
  
-    return innerVariable;
+    return "Funky";
   }
 }
 
