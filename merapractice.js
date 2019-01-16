6a7aed61cef1b4c2c9c24dbf8a55a8170d31ecb3
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-16 05:59:31 +0000

    Automatically backed up by Learn

--- merapractice.js
+++ merapractice.js
@@ -10,11 +10,9 @@ function outerFunction() {
 
 var myScope = outerFunction();
  
-// the stringified version of `innerScope()`
-myScope;
-Cool! What happens if we try to call innerScope() directly?
 
-innerScope();
-This will throw the error undefined is not a function (very helpful, JavaScript). But if we call myScope (the variable to which we assigned the output of outerFunction()):
+myScope
 
-myScope();
+innerScope()
+
+myScope()
